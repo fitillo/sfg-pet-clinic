c79dd218cbac5693eb82e4643eae00cf8b613cf9
adolfogarlez <adolfogarlez@gmail.com>
2020-02-18 15:25:38 +0800

    Add JPA annotations to Vet and related classes. closes #33.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,14 +1,26 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vet")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vets_specialities",
+        joinColumns = @JoinColumn(name = "vet_id"),
+        inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
         return specialities;
+    }
+
+    public boolean addSpeciality(Speciality speciality) {
+        speciality.addVet(this);
+        return this.specialities.add(speciality);
     }
 
     public void setSpecialities(Set<Speciality> specialities) {
